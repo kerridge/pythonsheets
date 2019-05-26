893a56e2c572802c7e2a5bce750ab076f4d58e01
Sam Kerridge <kerridge@Sams-MacBook-Pro.local>
2019-05-26 17:22:44 +1200

    added other weather types

--- models/field.py
+++ models/field.py
@@ -1,7 +1,12 @@
 class Field:
-    def __init__(self, name):
+    def __init__(self, name, url):
         self.name = name
+        self.url = url
 
     name = ''
+    url = ''
     rain = []
     snow = []
+    max_temp = []
+    min_temp = []
+    wind_chill = []
